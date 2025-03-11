713355a1ceaec4ddd93adc288f2457869ba27b1e
trimory <43339605+trimory@users.noreply.github.com>
2025-03-11 13:29:26 +0100

    Add project files.

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboration_2_Tim_Ringvold__David_Berg_Överby.Controllers
+{
+    public class IndexController : Controller
+    {
+        string jsonResult = string.Empty;
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Accept.Add("Authorization", "Bearer Token");
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboration_2_Tim_Ringvold__David_Berg_Överby.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
